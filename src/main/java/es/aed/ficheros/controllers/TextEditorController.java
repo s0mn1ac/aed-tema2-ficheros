2fd5c5a0fe6edbd0424e183c61191d5f07a9a38d
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-02-10 19:32:33 +0000

    add: carga de archivos de texto

--- src/main/java/es/aed/ficheros/controllers/TextEditorController.java
+++ src/main/java/es/aed/ficheros/controllers/TextEditorController.java
@@ -2,10 +2,14 @@ package es.aed.ficheros.controllers;
 
 import es.aed.ficheros.models.TextEditorModel;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,6 +17,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 
 public class TextEditorController implements Initializable {
 
@@ -32,6 +37,33 @@ public class TextEditorController implements Initializable {
 
 	@FXML
 	private Button textEditorLoadButton;
+	
+	@FXML
+	private void onClickLoadButton(ActionEvent e) {
+		
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Seleccionar archivo");
+		
+		File selectedFile = fileChooser.showOpenDialog(null);
+		
+		Path pathFile=null;
+		
+		if (selectedFile != null) {
+			pathFile = selectedFile.toPath();
+			ArrayList<String> fileContent = (ArrayList<String>) textEditorModel.openFile(pathFile.toString());
+			textEditorTextArea.clear();
+			for (String line : fileContent) {
+				textEditorTextArea.appendText(line);
+			}
+		}
+
+	}
+	
+	@FXML
+	private void onClickSaveButton(ActionEvent e) {
+		
+		
+	}
 
 	public TextEditorController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/TextEditorView.fxml"));
@@ -44,8 +76,9 @@ public class TextEditorController implements Initializable {
 	}
 	
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
+		// TODO: Implementar
+		textEditorTextArea.setWrapText(true);		
 	}
 
 }
--- src/main/java/es/aed/ficheros/models/TextEditorModel.java
+++ src/main/java/es/aed/ficheros/models/TextEditorModel.java
@@ -1,8 +1,30 @@
 package es.aed.ficheros.models;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TextEditorModel {
 	
 	public TextEditorModel() {
+		
+	}
+	
+	public ArrayList<String> openFile(String path) {
+		
+		File file  = new File(path);
+		ArrayList<String> fileContent = new ArrayList<String>();
+		
+		try (Scanner sc = new Scanner(file)) {
+			while (sc.hasNextLine()) {
+				fileContent.add(sc.nextLine() + "\n");
+			}
+		} catch (FileNotFoundException x) {
+			x.printStackTrace();
+		}
+		
+		return fileContent;
 		
 	}
 
